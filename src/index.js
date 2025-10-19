1ed435cd7e9356f878b5202560045a6561bc7420
gustavords <22622146+gustavords@users.noreply.github.com>
2025-10-19 15:23:33 -0400

    Fix image load issue with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import "./styles.css";
+import { fuck } from "./sprites/resources";
+import fuckyou from "./fuck.png";
 
 
+const canvas = document.querySelector( `#game-canvas` );
+const ctx = canvas.getContext( `2d` );
 
+// const draw = () =>
+// {
+//   // const sky = resources.images.sky;
+//   // if ( sky.isLoaded )
+//   // {
+//   //   ctx.drawImage( sky.image, 0, 0 );
+//   // }
 
+//   // const img = new Image();
+//   // img.src = sky;
+//   // ctx.drawImage( img, 0, 0 );
+// }
 
+console.log( fuck );
+const img = new Image();
+img.src = fuckyou;
+ctx.drawImage( img, 0, 0 );
+
+document.body.appendChild(img);
+
+// setInterval( () =>
+// {
+//   console.log(`draw`);
+//   draw();
+
+// }, 300 );
---
+++ src/sprites/resources.js
@@ -0,0 +1,36 @@
+// import sky from `./sprites/spritesheet.png`;
+
+
+
+// class Resources
+// {
+//   constructor ()
+//   {
+//     this.toLoad = {
+//       sky: sky,
+//       // ground: `/sprites/ground.png`,
+//       // hero: `/sprites/hero-sheet.png`,
+//       // shadow: `/sprites/shadow.png`,
+//     }
+
+//     //bucket of images
+//     this.images = {}
+
+//     Object.keys( this.toLoad ).forEach( ( key ) =>
+//     {
+//       console.log(sky)
+//       const img = new Image();
+//       img.src = this.toLoad[ key ];
+//       this.images[ key ] = {
+//         image: img,
+//         isLoaded: false,
+//       }
+//       img.onLoad = () =>
+//       {
+//         this.images[ key ].isLoaded = true;
+//       }
+//     } );
+//   }
+// }
+export const fuck = `fuck you`;
+// // export const resources = new Resources();
