ae5f5b6f71d7e12f901c2400125f096fad320f36
gustavords <22622146+gustavords@users.noreply.github.com>
2025-10-19 20:10:55 -0400

    Add sprite.js and vector2.js. Display sprite through class. Actually worked! Happy!

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 import "./styles.css";
 import { resources } from "./resources.js";
-
+import { Sprite } from "./sprite.js";
+import { Vector2 } from "./vector2.js";
 
 const canvas = document.querySelector( `#game-canvas` );
 const ctx = canvas.getContext( `2d` );
 
+
+const skySprite = new Sprite( {
+  resource: resources.images.sky,
+  frameSize: new Vector2( 320, 180 ),
+} )
+const groundSprite = new Sprite( {
+  resource: resources.images.ground,
+  frameSize: new Vector2( 320, 180 ),
+} )
+const heroSprite = new Sprite( {
+  resource: resources.images.hero,
+  frameSize: new Vector2( 32, 32 ),
+  hFrames: 3,
+  vFrame: 8,
+  frame: 1,
+} )
+
+const heroPos = new Vector2( 16 * 5, 16 * 5 );
+
+// console.log( skySprite.frame );
+
+
 const draw = () =>
 {
-  const sky = resources.images.sky;
-  if ( !sky.isLoaded )
-  {
-    ctx.drawImage( sky.image, 0, 0 );
-  }
 
-  const ground = resources.images.ground;
-  if ( !ground.isLoaded )
-  {
-    ctx.drawImage( ground.image, 0, 0 );
-  }
+  skySprite.drawImage( ctx, 0, 0 );
+  groundSprite.drawImage( ctx, 0, 0 );
+  heroSprite.drawImage( ctx, heroPos.x, heroPos.y );
+
+
 }
 
+// const hero = new Sprite( {
+//   resource: resources.images.hero,
+//   hFrames: 3,
+//   vFrames: 8,
+//   frame: 1,
+// } );
+
+// hero.buildFrameMap();
 
 setInterval( () =>
 {
---
+++ src/sprite.js
@@ -0,0 +1,76 @@
+import { Vector2 } from "./vector2.js";
+
+export class Sprite
+{
+  constructor ( {
+    resource, //image we wanna draw
+    frameSize, //size of  the crop of image
+    hFrames, //horizontal arrangement
+    vFrames, //vertical arrangement
+    frame, //frames we want to show
+    scale, //how large to draw
+    position,  //where to draw it (top left corner)
+  } )
+  {
+    this.resource = resource;
+    this.frameSize = frameSize ?? new Vector2( 16, 16 );
+    this.hFrames = hFrames ?? 1;
+    this.vFrames = vFrames ?? 1;
+    this.frame = frame ?? 0;
+    this.frameMap = new Map();
+    this.scale = scale ?? 1;
+    this.position = position ?? new Vector2( 0, 0 );
+    this.buildFrameMap();
+  }
+
+  buildFrameMap ()
+  {
+    let frameCount = 0;
+    for ( let v = 0; v < this.vFrames; v++ )
+    {
+      for ( let h = 0; h < this.hFrames; h++ )
+      {
+        // console.log( `frame`, h, v );
+        this.frameMap.set(
+          frameCount,
+          new Vector2( this.frameSize.x * h, this.frameSize.y * v ),
+
+        );
+        frameCount++;
+
+      }
+    }
+  }
+
+  drawImage ( ctx, x, y )
+  {
+    // console.log( this.resource.isLoaded );
+    // if ( this.resource.isLoaded ) { return; }
+
+    //find correct sprite sheet
+    let frameCoordX = 0;
+    let frameCoordY = 0;
+
+    const frame = this.frameMap.get( this.frame );
+    if ( frame )
+    {
+      frameCoordX = frame.x;
+      frameCoordY = frame.y;
+    }
+
+    const frameSizeX = this.frameSize.x;
+    const frameSizeY = this.frameSize.y;
+
+    ctx.drawImage(
+      this.resource.image, //instance of an image
+      frameCoordX, //
+      frameCoordY, //Top Y of frame
+      frameSizeX, //how much to crop from sprite sheet
+      frameSizeY, //how much to crop from sprite sheet
+      y, //where on canvas
+      x, //where on canvas
+      frameSizeX * this.scale, //how large to scale is it
+      frameSizeY * this.scale  //how large to scale is it
+    )
+  }
+}
